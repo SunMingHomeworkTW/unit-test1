29fbdf3781f61c38bc2735722307263db3458830
SunMing <1229536801@qq.com>
2018-04-21 14:33:41 +0800

    为RandomIntGenerator类编写测试方法testGenerateNums，测试程序能否随机生成有效的字符串数据

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,32 @@
 package tw.core;
 
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+    private RandomIntGenerator randomIntGenerator;
 
+
+    @Before
+    public void setUp() throws Exception {
+        randomIntGenerator=new RandomIntGenerator();
+    }
+
+    @Test
+    public void testGenerateNums() {
+        int digitmax=9;
+        int numbersOfNeed=4;
+
+        String generatedNumsStr=randomIntGenerator.generateNums(digitmax,numbersOfNeed);
+
+        assertTrue(new InputValidator().validate(generatedNumsStr));
+    }
 }
