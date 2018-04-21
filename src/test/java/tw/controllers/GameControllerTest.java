c32f4b6a1c0654ed85bb6e17d7e4aa86de49ae88
SunMing <1229536801@qq.com>
2018-04-21 14:50:00 +0800

    为GameController类编写了测试方法testBeginGame和testPlay,测试游戏能否正确开始，能否正确执行。其中，testPlay最终没能正确写出来

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -1,8 +1,61 @@
 package tw.controllers;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.Game;
+import tw.views.GameView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+
 /**
  * 在GameControllerTest文件中完成GameController中对应的单元测试
  */
 public class GameControllerTest {
+    private GameController gameController;
+    private Game game;
+    private GameView gameView;
 
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() throws Exception {
+        game=mock(Game.class);
+        gameView=spy(GameView.class);
+        gameController=new GameController(game,gameView);
+
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
+
+    @Test
+    public void testBeginGame() throws IOException {
+        gameController.beginGame();
+        assertEquals("------Guess Number Game, You have 6 chances to guess!  ------\r\n",systemOut());
+    }
+
+    @Test
+    public void testPlay() throws IOException{
+//        when(game.checkCoutinue()).thenReturn(true).thenReturn(false);
+//
+//        Answer actualAnswer=Answer.createAnswer("1 2 3 4");
+//
+//        InputCommand inputCommand=mock(GuessInputCommand.class);
+//        when(inputCommand.input()).thenReturn(actualAnswer);
+//
+//        Mockito.doCallRealMethod().when(game).guess(actualAnswer);
+////        when(game.guess(actualAnswer)).thenReturn(new GuessResult("4A0B",actualAnswer));
+//
+//        gameController.play(inputCommand);
+//
+//        assertEquals("Game Status: success\r\n",systemOut());
+    }
 }
