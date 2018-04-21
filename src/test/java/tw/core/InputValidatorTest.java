96bbc898be57777f8434baf9d34abcf61859d6d3
SunMing <1229536801@qq.com>
2018-04-21 14:31:22 +0800

    为InputValidator类编写测试方法testValidate，对输入字符串的有效性进行测试

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,35 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+    private InputValidator inputValidator;
 
+    @Before
+    public void setUp() throws Exception {
+        inputValidator=new InputValidator();
+    }
+
+    @Test
+    public void testValidate() {
+        String numStr1="1 2 3 4";
+        String numStr2="1 2 3";
+        String numStr3="1 2 3 4 5";
+        String numStr4="1 1 3 4";
+        String numStr5="10 2 3 4";
+
+        assertTrue(inputValidator.validate(numStr1));
+        assertFalse(inputValidator.validate(numStr2));
+        assertFalse(inputValidator.validate(numStr3));
+        assertFalse(inputValidator.validate(numStr4));
+        assertFalse(inputValidator.validate(numStr5));
+    }
 }
