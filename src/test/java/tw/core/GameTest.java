9c9ae7c0354c3931c3ef98e5d4968337fd1f23d4
SunMing <1229536801@qq.com>
2018-04-21 14:44:24 +0800

    为Game类编写一系列的测试方法，测试程序能否对所猜数据产生正确的输出，能否输出相应的历史，能否对本次游戏的状态有正确的判断等等

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -1,10 +1,128 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在GameTest文件中完成Game中对应的单元测试
  */
 
 
 public class GameTest {
+    private Game game;
 
+    @Before
+    public void setUp() throws Exception {
+        AnswerGenerator answerGenerator=mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(Answer.createAnswer("1 2 3 4"));
+        game=new Game(answerGenerator);
+    }
+
+    @Test
+    public void testGuess() throws OutOfRangeAnswerException {
+        assertEquals("4A0B",game.guess(Answer.createAnswer("1 2 3 4")).getResult());
+        assertEquals("0A4B",game.guess(Answer.createAnswer("4 3 2 1")).getResult());
+        assertEquals("0A0B",game.guess(Answer.createAnswer("5 6 7 8")).getResult());
+        assertEquals("2A2B",game.guess(Answer.createAnswer("1 2 4 3")).getResult());
+    }
+
+    @Test
+    public void testGuessHistory() {
+        assertEquals(0, game.guessHistory().size());
+
+        game.guess(Answer.createAnswer("1 2 3 4"));
+        assertEquals(1, game.guessHistory().size());
+    }
+
+    @Test
+    public void testCheckStatus_success_first() {
+        game.guess(Answer.createAnswer("1 2 3 4"));
+        assertEquals("success",game.checkStatus());
+    }
+
+    @Test
+    public void testCheckStatus_success_second(){
+        game.guess(Answer.createAnswer("5 6 7 8"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("1 2 3 4"));
+        assertEquals("success",game.checkStatus());
+    }
+    @Test
+    public void testCheckStatus_success_sixth(){
+        game.guess(Answer.createAnswer("5 6 7 8"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("1 2 3 9"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("2 1 3 9"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("1 2 9 3"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("4 3 2 1"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("1 2 3 4"));
+        assertEquals("success",game.checkStatus());
+    }
+    @Test
+    public void testCheckStatus_fail(){
+        game.guess(Answer.createAnswer("5 6 7 8"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("1 2 3 9"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("2 1 3 9"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("1 2 9 3"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("4 3 2 1"));
+        assertEquals("continue",game.checkStatus());
+
+        game.guess(Answer.createAnswer("1 2 3 5"));
+        assertEquals("fail",game.checkStatus());
+    }
+
+    @Test
+    public void testCheckContinue_success() {
+        game.guess(Answer.createAnswer("1 2 3 4"));
+        assertFalse(game.checkCoutinue());
+    }
+
+    @Test
+    public void testCheckContinue_sixth() {
+        assertTrue(game.checkCoutinue());
+
+        game.guess(Answer.createAnswer("5 6 7 8"));
+        assertTrue(game.checkCoutinue());
+
+        game.guess(Answer.createAnswer("1 2 3 9"));
+        assertTrue(game.checkCoutinue());
+
+        game.guess(Answer.createAnswer("2 1 3 9"));
+        assertTrue(game.checkCoutinue());
+
+        game.guess(Answer.createAnswer("1 2 9 3"));
+        assertTrue(game.checkCoutinue());
+
+        game.guess(Answer.createAnswer("4 3 2 1"));
+        assertTrue(game.checkCoutinue());
+
+        game.guess(Answer.createAnswer("1 2 3 4"));
+        assertFalse(game.checkCoutinue());
+    }
 }
