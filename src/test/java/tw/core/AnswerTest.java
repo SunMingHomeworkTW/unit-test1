4ce204fdfe242fd483094e700862e8ce44bb8f73
SunMing <1229536801@qq.com>
2018-04-22 22:31:52 +0800

    重构AnswerTest类

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -16,84 +16,60 @@ import static org.junit.Assert.assertEquals;
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
-    private Answer answer;
+    private Answer actualAnswer;
+    private Answer inputAnswer;
 
     @Before
-    public void setUp() throws Exception {
-        answer=new Answer();
+    public void setUp() {
+        actualAnswer =Answer.createAnswer("1 2 3 4");
+        inputAnswer=new Answer();
     }
 
     @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+    public ExpectedException exception = ExpectedException.none();
 
     @Test
-    public void testCreateAnswer() {
+    public void should_createAnswer_return_answer_when_input_string() {
         assertEquals(Answer.createAnswer("1 2 3 4").toString(),"1 2 3 4");
     }
 
     @Test
-    public void testValidate() throws OutOfRangeAnswerException {
-        List<String> numList1=Arrays.asList("1","2","3");
-        answer.setNumList(numList1);
-        expectedException.expect(OutOfRangeAnswerException.class);
-        answer.validate();
-
-        List<String> numList2=Arrays.asList("1","2","3","4","5");
-        answer.setNumList(numList2);
-        expectedException.expect(OutOfRangeAnswerException.class);
-        answer.validate();
-
-        List<String> numList3=Arrays.asList("1","1","3","4");
-        answer.setNumList(numList3);
-        expectedException.expect(OutOfRangeAnswerException.class);
-        answer.validate();
-
-        List<String> numList4=Arrays.asList("11","2","3","4");
-        answer.setNumList(numList4);
-        expectedException.expect(OutOfRangeAnswerException.class);
-        answer.validate();
+    public void should_validate_throw_OutOfRangeAnswerException_when_answer_contains_same_digits() throws OutOfRangeAnswerException {
+        actualAnswer.setNumList(Arrays.asList("1","1","3","4"));
+        exception.expect(OutOfRangeAnswerException.class);
+        exception.expectMessage("Answer format is incorrect");
+        actualAnswer.validate();
     }
 
     @Test
-    public void testCheck() {
-        Answer inputAnswer=new Answer();
-
-        List<String> numList=Arrays.asList("1","2","3","4");
-        answer.setNumList(numList);
-
-        List<String> numList1=Arrays.asList("1","2","3","4");
-        inputAnswer.setNumList(numList1);
-        assertArrayEquals(new int[]{4,0},answer.check(inputAnswer).getValue());
-
-        List<String> numList2=Arrays.asList("4","3","2","1");
-        inputAnswer.setNumList(numList2);
-        assertArrayEquals(new int[]{0,4},answer.check(inputAnswer).getValue());
-
-        List<String> numList3=Arrays.asList("5","6","7","8");
-        inputAnswer.setNumList(numList3);
-        assertArrayEquals(new int[]{0,0},answer.check(inputAnswer).getValue());
-
-        List<String> numList4=Arrays.asList("1","2","4","3");
-        inputAnswer.setNumList(numList4);
-        assertArrayEquals(new int[]{2,2},answer.check(inputAnswer).getValue());
+    public void should_validate_throw_OutOfRangeAnswerException_when_answer_contains_digit_greater_than_9() throws OutOfRangeAnswerException {
+        exception.expect(OutOfRangeAnswerException.class);
+        exception.expectMessage("Answer format is incorrect");
+        actualAnswer.setNumList(Arrays.asList("1", "2", "3","10"));
+        actualAnswer.validate();
     }
 
     @Test
-    public void testGetIndexOfNum() {
-        List<String> numList=Arrays.asList("1","2","3","4");
-        answer.setNumList(numList);
-
-        assertEquals(0,answer.getIndexOfNum("1"));
-        assertEquals(3,answer.getIndexOfNum("4"));
-        assertEquals(-1,answer.getIndexOfNum("5"));
-        assertEquals(-1,answer.getIndexOfNum("a"));
+    public void should_check_return_record_4_and_0_when_inputAnswer_all_right() {
+        inputAnswer.setNumList(Arrays.asList("1","2","3","4"));
+        assertArrayEquals(new int[]{4,0}, actualAnswer.check(inputAnswer).getValue());
     }
 
     @Test
-    public void testToString() {
-        List<String> numList=Arrays.asList("1","2","3","4");
-        answer.setNumList(numList);
+    public void should_check_return_record_0_and_4_when_inputAnswer__all_in_wrong_positions() {
+        inputAnswer.setNumList(Arrays.asList("4","3","2","1"));
+        assertArrayEquals(new int[]{0,4}, actualAnswer.check(inputAnswer).getValue());
+    }
 
-        assertEquals("1 2 3 4",answer.toString());
+    @Test
+    public void should_check_return_record_0_and_0_when_inputAnswer_all_wrong() {
+        inputAnswer.setNumList(Arrays.asList("5","6","7","8"));
+        assertArrayEquals(new int[]{0,0}, actualAnswer.check(inputAnswer).getValue());
+    }
+
+    @Test
+    public void should_check_return_record_4_and_0_when_inputAnswer_has_2_right_digits_and_2_in_wrong_positions() {
+        inputAnswer.setNumList(Arrays.asList("1","2","4","3"));
+        assertArrayEquals(new int[]{2,2}, actualAnswer.check(inputAnswer).getValue());
     }
 }
