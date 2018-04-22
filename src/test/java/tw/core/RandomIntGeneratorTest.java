8779f9dc614ef382c9147e0b05f1448f553f1997
SunMing <1229536801@qq.com>
2018-04-22 21:08:08 +0800

    重构RandomIntGeneratorTest类

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -2,10 +2,16 @@ package tw.core;
 
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import tw.core.generator.RandomIntGenerator;
 import tw.validator.InputValidator;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -13,20 +19,44 @@ import static org.junit.Assert.assertTrue;
  */
 public class RandomIntGeneratorTest {
     private RandomIntGenerator randomIntGenerator;
-
-
+    
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         randomIntGenerator=new RandomIntGenerator();
+    }
+    
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void should_throw_IllegalArgumentException_when_digitmax_less_than_numbersOfNeed() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Can't ask for more numbers than are available");
+        randomIntGenerator.generateNums(2,4);
     }
 
     @Test
-    public void testGenerateNums() {
-        int digitmax=9;
-        int numbersOfNeed=4;
+    public void should_generate_digit_number_equals_numbersOfNeed() {
+        String generateNumsStr = randomIntGenerator.generateNums(9,5);
+        assertEquals(9,generateNumsStr.length());
+    }
 
-        String generatedNumsStr=randomIntGenerator.generateNums(digitmax,numbersOfNeed);
+    @Test
+    public void should_generate_different_digits() {
+        String generateNumsStr = randomIntGenerator.generateNums(9,4);
+        assertEquals(4, Arrays.stream(generateNumsStr.split(" ")).distinct().count());
+    }
 
-        assertTrue(new InputValidator().validate(generatedNumsStr));
+    @Test
+    public void should_generate_digits_less_than_digmax() {
+        String generateNumsStr = randomIntGenerator.generateNums(9,4);
+        assertTrue( Arrays.stream(generateNumsStr.split(" ")).allMatch(i->Integer.valueOf(i)<=9));
+    }
+
+    @Test
+    public void should_generate_random_digits() {
+        String generateNumsStr1 = randomIntGenerator.generateNums(9,4);
+        String generateNumsStr2 = randomIntGenerator.generateNums(9,4);
+        assertNotEquals(generateNumsStr1,generateNumsStr2);
     }
 }
