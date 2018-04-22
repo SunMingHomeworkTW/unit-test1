9472790cc90c87409f89e960e9130c28c6f5bd20
SunMing <1229536801@qq.com>
2018-04-22 22:10:23 +0800

    AnswerGeneratorTest类

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,7 +1,9 @@
 package tw.core.generator;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import tw.core.Answer;
 import tw.core.exception.OutOfRangeAnswerException;
 
@@ -17,16 +19,35 @@ public class AnswerGeneratorTest {
     private RandomIntGenerator randomIntGenerator;
 
     @Before
-    public void setUp() throws Exception {
-        randomIntGenerator=mock(RandomIntGenerator.class);
-        answerGenerator=new AnswerGenerator(randomIntGenerator);
+    public void setUp() {
+        randomIntGenerator = mock(RandomIntGenerator.class);
+        answerGenerator = new AnswerGenerator(randomIntGenerator);
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void should_return_the_answer_same_as_generateNums() throws OutOfRangeAnswerException {
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn("1 2 3 4");
+
+        assertEquals(Answer.createAnswer("1 2 3 4").toString(), answerGenerator.generate().toString());
     }
 
     @Test
-    public void testGenerate() throws OutOfRangeAnswerException {
-        when(randomIntGenerator.generateNums(9,4)).thenReturn("1 2 3 4");
+    public void should_throw_OutOfRangeAnswerException_when_generateNums_contains_same_digits() throws OutOfRangeAnswerException {
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn("1 1 2 3");
+        exception.expect(OutOfRangeAnswerException.class);
+        exception.expectMessage("Answer format is incorrect");
+        answerGenerator.generate();
+    }
 
-        assertEquals(Answer.createAnswer("1 2 3 4").toString(),answerGenerator.generate().toString());
+    @Test
+    public void should_throw_OutOfRangeAnswerException_when_generateNums_contains_digit_greater_than_9() throws OutOfRangeAnswerException {
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn("1 2 3 10");
+        exception.expect(OutOfRangeAnswerException.class);
+        exception.expectMessage("Answer format is incorrect");
+        answerGenerator.generate();
     }
 }
 
